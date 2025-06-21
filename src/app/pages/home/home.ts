64e7afa7c521c069c23accf3e03b5458cdd8796f
Luiz Ricardo <luu.izz@hotmail.com>
2025-06-20 22:26:37 -0300

    feat: adicionado container, alterado label do link e adicionado botão e lógica de ver mais.

--- src/app/pages/home/home.ts
+++ src/app/pages/home/home.ts
@@ -2,23 +2,32 @@ import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../../services/pokemon';
 import { PokemonListResponse } from '../../../types/pokeTypes';
 import { RouterLink } from '@angular/router';
+import { Container } from '../../components/container/container';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, Container],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home implements OnInit {
   pokemons: PokemonListResponse['results'] = [];
+  countPagination: number = 20;
 
   constructor(private pokemonService: Pokemon) {}
 
   ngOnInit(): void {
-    this.pokemonService
-      .getPokemons(20)
-      .subscribe((res: PokemonListResponse) => {
-        this.pokemons = res.results;
-      });
+    this.loadPokemons();
+  }
+
+  loadPokemons() {
+    this.pokemonService.getPokemons(this.countPagination).subscribe((res) => {
+      this.pokemons = res.results;
+    });
+  }
+
+  showMorePokemons(): void {
+    this.countPagination += 20;
+    this.loadPokemons();
   }
 }
